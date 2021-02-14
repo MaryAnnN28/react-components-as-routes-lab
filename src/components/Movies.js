70c6d081148722abf6f5d094ecdffea39a4d64ea
MaryAnnN28 <maryann.navarrete@gmail.com>
2021-02-14 14:10:16 -0500

    done

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,21 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
+
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      
+      {movies.map(movie => <div>
+        <h3>{movie.title}</h3>
+        Time: {movie.time} min.
+        <ul>
+          {movie.genres.map(genre => <li>{genre}</li>)}
+        </ul>
+        <br />
+      </div>)}
+      
     </div>
   );
 };
